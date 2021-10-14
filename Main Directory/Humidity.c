c275ca7e4d6d28fa8824c1de169af101544519c6
JamesManown <manownjdm@gmail.com>
2021-10-14 09:55:14 -0400

    Update Humidity.c

--- Main Directory/Humidity.c
+++ Main Directory/Humidity.c
@@ -1,2 +1,143 @@
-#include "humidity.h"
+/*
+* Humidity file designed for DHT11
+* 
+* CS 321, Fall 2021. Sensor Collection.
+*/
 
+#include "humidity.h"
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MAXTIMINGS 85
+//#define DHTPIN 1
+
+int dht11_dat[5] = {0,0,0,0,0};
+int DHTPIN;
+
+int setPin(int pinNum) {
+	DHTPIN = pinNum;
+}
+
+//Similar to method below it, but returns the humidity as an int.
+int get_humidity()
+{
+	int returnHumidity = -1;
+	char humStr[2];
+	uint8_t laststate = HIGH;
+	uint8_t counter = 0;
+	uint8_t j = 0, i;
+	float f;
+
+	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+	// pull pin down for 18 milliseconds
+	pinMode(DHTPIN, OUTPUT);
+	digitalWrite(DHTPIN, LOW);
+	delay(18);
+	// then pull it up for 40 microseconds
+	digitalWrite(DHTPIN, HIGH);
+	delayMicroseconds(40); 
+	// prepare to read the pin
+	pinMode(DHTPIN, INPUT);
+
+	// detect change and read data
+	for ( i=0; i< MAXTIMINGS; i++) {
+		counter = 0;
+		while (digitalRead(DHTPIN) == laststate) {
+			counter++;
+			delayMicroseconds(1);
+			if (counter == 255) {
+				break;
+			}
+		}
+		laststate = digitalRead(DHTPIN);
+
+		if (counter == 255) break;
+
+		// ignore first 3 transitions
+		if ((i >= 4) && (i%2 == 0)) {
+			// shove each bit into the storage bytes
+			dht11_dat[j/8] <<= 1;
+			if (counter > 16)
+				dht11_dat[j/8] |= 1;
+			j++;
+		}
+	}
+
+	// check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+	// print it out if data is good
+	if ((j >= 40) && 
+			(dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)) ) {
+		f = dht11_dat[2] * 9. / 5. + 32;
+		printf("Humidity = %d.%d %% Temperature = %d.%d *C (%.1f *F)\n", 
+				dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f);
+	}
+	else
+	{
+		printf("Data not good, skip\n");
+	}
+	
+	sprintf(humStr, "%d%d", dht11_dat[2], dht11_dat[3]);
+	return strtol(humStr, null, 10);
+}
+
+
+
+void read_dht11_dat()
+{
+	uint8_t laststate = HIGH;
+	uint8_t counter = 0;
+	uint8_t j = 0, i;
+	float f;
+
+	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+	// pull pin down for 18 milliseconds
+	pinMode(DHTPIN, OUTPUT);
+	digitalWrite(DHTPIN, LOW);
+	delay(18);
+	// then pull it up for 40 microseconds
+	digitalWrite(DHTPIN, HIGH);
+	delayMicroseconds(40); 
+	// prepare to read the pin
+	pinMode(DHTPIN, INPUT);
+
+	// detect change and read data
+	for ( i=0; i< MAXTIMINGS; i++) {
+		counter = 0;
+		while (digitalRead(DHTPIN) == laststate) {
+			counter++;
+			delayMicroseconds(1);
+			if (counter == 255) {
+				break;
+			}
+		}
+		laststate = digitalRead(DHTPIN);
+
+		if (counter == 255) break;
+
+		// ignore first 3 transitions
+		if ((i >= 4) && (i%2 == 0)) {
+			// shove each bit into the storage bytes
+			dht11_dat[j/8] <<= 1;
+			if (counter > 16)
+				dht11_dat[j/8] |= 1;
+			j++;
+		}
+	}
+
+	// check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+	// print it out if data is good
+	if ((j >= 40) && 
+			(dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)) ) {
+		f = dht11_dat[2] * 9. / 5. + 32;
+		printf("Humidity = %d.%d %% Temperature = %d.%d *C (%.1f *F)\n", 
+				dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f);
+	}
+	else
+	{
+		printf("Data not good, skip\n");
+	}
+}
